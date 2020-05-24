23ba36b5d80cdf647dbca857e4d8dbe821b679b6
srikanth2001 <47571970+srikanth2001@users.noreply.github.com>
2020-05-24 13:56:36 -0400

    Add files via upload

---
+++ ee18btech11023.py
@@ -0,0 +1,27 @@
+#by K Srikanth
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#if using termux
+import subprocess
+import shlex
+#end if
+
+data=np.loadtxt('ee18btech11023.dat')
+plt.plot(data[:,0],data[:,1])
+plt.grid()
+plt.xlabel("time")
+plt.ylabel("Amplitude")
+plt.title('Spice simulation for feedback amplifier')
+#plt.show()
+#if using termux
+
+
+
+plt.savefig('../../figs/EE18BTECH11023/ee18btech11023_output.pdf')
+plt.savefig('../../figs/EE18BTECH11023/ee18btech11023_output.eps')
+subprocess.run(shlex.split("termux-open ./figs/EE18BTECH11023/ee18btech11023_output.pdf"))
+#else
+#plt.show()
